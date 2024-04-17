cc25f94db1b1ffbf50385d8747e8815e43f6dafc
Sean Hill <seansghost@gmail.com>
2024-04-16 18:19:00 -0700

    Roll back to TF 1.7 due to bug

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.8.0"
+  required_version = ">= 1.7.0" # TODO: Update to 1.8 after bugfix
   required_providers {
     terraform = {
       source = "terraform.io/builtin/terraform"
